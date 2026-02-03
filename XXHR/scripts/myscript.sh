988c95ff85cd708a643a49b40973bae8995a2607
Ram-Katta <56863557+Ram-Katta@users.noreply.github.com>
2026-02-03 14:23:35 -0600

    Add files via upload

---
+++ XXHR/scripts/myscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "XXHR_TOP: $XXHR_TOP"
+cd "$XXHR_TOP/mds"
+
+echo "Working directory: $(pwd)"
+echo ""
+
+echo "unzipping zipped_files.zip"
+unzip -o -q zipped_files.zip
+echo ""
+
+ls -ltra
+# asdasd
+# asdasd
+# asdasd
