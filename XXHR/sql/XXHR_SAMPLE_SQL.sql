0ceaa8ed09ca9fb0895b5d252b42c43cd1264bed
Ram-Katta <56863557+Ram-Katta@users.noreply.github.com>
2026-05-22 12:45:55 -0500

    Enhance SQL script with output messages
    Added output messages to indicate script start and completion.

--- XXHR/sql/XXHR_SAMPLE_SQL.sql
+++ XXHR/sql/XXHR_SAMPLE_SQL.sql
@@ -1,6 +1,10 @@
+SET SERVEROUTPUT ON
+
 BEGIN
-DBMS_STATS.GATHER_TABLE_STATS ('TARGET','DUMMY');
+    DBMS_OUTPUT.PUT_LINE('Script started...');
+    DBMS_OUTPUT.PUT_LINE('DB Time: ' || TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'));
+    DBMS_OUTPUT.PUT_LINE('Script completed successfully.');
 END;
 /
-EXIT;
---comment
+
+EXIT
